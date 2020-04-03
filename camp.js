efecd0b98bf43e362af31f99133c65482abf29d5
rahulkiller104 <56788144+rahulkiller104@users.noreply.github.com>
2020-04-03 13:18:57 +0530

    object not found

---
+++ camp.js
@@ -0,0 +1,56 @@
+var express   =require("express");
+var app       =express();
+var bodyparser=require("body-parser");
+var mongoose=require("mongoose");
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("views"));
+
+//schema setup
+var campgroundschema=new mongoose.Schema({
+	name:String,
+image:String});
+var campground=mongoose.model("campground",campgroundschema);
+
+
+
+var campground=[{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
+{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
+,{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
+{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
+{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
+{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
+{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
+{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
+{name:"mountain heel",image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"}
+	]
+
+app.get("/",function(req,res){
+	res.render("landing.ejs");
+});
+app.get("/campground",function(req,res){
+res.render("campground.ejs",{campground:campground});
+
+
+
+});
+app.post("/campground",function(req,res){
+	//get data from from
+	var name =req.body.name;
+	var image=req.body.image;
+	var newcampground={name: name,image: image};
+	
+	campground.push(newcampground);
+	res.redirect("/campground");
+	
+});
+
+	app.get("/campground/new",function(req,res){
+	//get data from from
+	res.render("new.ejs");   
+	
+});
+
+app.listen(3000,function(){
+	console.log("server is started");
+});
